a4b7b7bedd7a8debacfa3c65a50351b9564328b6
aneins <zengxin.jiao@tum.de>
2024-03-18 14:41:38 +0100

    分离出 accoslam

---
+++ ActiveCoSLAM/ac_decoder.py
@@ -0,0 +1,289 @@
+# Package imports
+
+import torch
+import torch.nn as nn
+import tinycudann as tcnn
+
+
+class SDFNet(nn.Module):
+    def __init__(self, config, input_ch=3, geo_feat_dim=15, hidden_dim=64, num_layers=2):
+        super(SDFNet, self).__init__()
+        self.config = config
+        self.input_ch = input_ch
+        self.geo_feat_dim = geo_feat_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.model = self.get_model(tcnn_network=config['decoder']['tcnn_network'])
+
+    def forward(self, x, return_geo=True):
+        out = self.model(x)
+
+        if return_geo:  # return feature
+            return out
+        else:
+            return out[..., :1]
+
+    def get_model(self, tcnn_network=False):
+        #TUM.yaml: tcnn_network=False
+        if tcnn_network:
+            print('SDF net: using tcnn')
+            return tcnn.Network(
+                n_input_dims=self.input_ch,
+                n_output_dims=1 + self.geo_feat_dim,
+                network_config={
+                    "otype": "FullyFusedMLP",
+                    "activation": "ReLU",
+                    "output_activation": "None",
+                    "n_neurons": self.hidden_dim,
+                    "n_hidden_layers": self.num_layers - 1,
+                },
+                # dtype=torch.float
+            )
+        else:
+            sdf_net = []
+            for l in range(self.num_layers):  # num_layers=2
+                if l == 0:
+                    in_dim = self.input_ch
+                else:
+                    in_dim = self.hidden_dim
+
+                if l == self.num_layers - 1:
+                    if self.config['active']['active']:
+                        out_dim = 1 + self.geo_feat_dim + 1   # 1 sigma + 15 SH features for color + 1 beta ---> 16
+                    else:
+                        out_dim = 1 + self.geo_feat_dim
+                else:
+                    out_dim = self.hidden_dim
+
+                sdf_net.append(nn.Linear(in_dim, out_dim, bias=False))
+                if l != self.num_layers - 1:
+                    sdf_net.append(nn.ReLU(inplace=True))
+
+            return nn.Sequential(*nn.ModuleList(sdf_net))
+
+
+class ColorNet(nn.Module):
+    def __init__(self, config, input_ch=4, geo_feat_dim=15, 
+                hidden_dim_color=64, num_layers_color=3):
+        super(ColorNet, self).__init__()
+        self.config = config
+        self.input_ch = input_ch
+        self.geo_feat_dim = geo_feat_dim
+        self.hidden_dim_color = hidden_dim_color
+        self.num_layers_color = num_layers_color
+
+        self.model = self.get_model(config['decoder']['tcnn_network'])
+    
+    def forward(self, input_feat):
+        # h = torch.cat([embedded_dirs, geo_feat], dim=-1)
+        return self.model(input_feat)
+    
+    def get_model(self, tcnn_network=False):
+        if tcnn_network:
+            print('Color net: using tcnn')
+            return tcnn.Network(
+                n_input_dims=self.input_ch + self.geo_feat_dim,
+                n_output_dims=3,
+                network_config={
+                    "otype": "FullyFusedMLP",
+                    "activation": "ReLU",
+                    "output_activation": "None",
+                    "n_neurons": self.hidden_dim_color,
+                    "n_hidden_layers": self.num_layers_color - 1,
+                },
+                #dtype=torch.float
+            )
+
+        color_net =  []
+        for l in range(self.num_layers_color):
+            if l == 0:
+                in_dim = self.input_ch + self.geo_feat_dim
+            else:
+                in_dim = self.hidden_dim_color
+            
+            if l == self.num_layers_color - 1:
+                out_dim = 3 # 3 rgb
+            else:
+                out_dim = self.hidden_dim_color
+            
+            color_net.append(nn.Linear(in_dim, out_dim, bias=False))
+            if l != self.num_layers_color - 1:
+                color_net.append(nn.ReLU(inplace=True))
+
+        return nn.Sequential(*nn.ModuleList(color_net))
+
+
+class ColorSDFNet(nn.Module):
+    '''
+    Color grid + SDF grid
+    # 允许颜色网络直接访问原始的颜色嵌入信息(ColorSDFNet的特点)
+
+    '''
+    def __init__(self, config, input_ch=3, input_ch_pos=12):
+        super(ColorSDFNet, self).__init__()
+        self.config = config
+        self.sdf_net = SDFNet(config,
+                input_ch=input_ch+input_ch_pos,
+                geo_feat_dim=config['decoder']['geo_feat_dim'],
+                hidden_dim=config['decoder']['hidden_dim'],
+                num_layers=config['decoder']['num_layers'])
+        self.color_net = ColorNet(config, 
+                input_ch=input_ch+input_ch_pos, 
+                geo_feat_dim=config['decoder']['geo_feat_dim'], 
+                hidden_dim_color=config['decoder']['hidden_dim_color'], 
+                num_layers_color=config['decoder']['num_layers_color'])
+
+            
+    def forward(self, embed, embed_pos, embed_color):
+
+        if embed_pos is not None:
+            h = self.sdf_net(torch.cat([embed, embed_pos], dim=-1), return_geo=True) 
+        else:
+            h = self.sdf_net(embed, return_geo=True)
+
+        if config['active']['active']:
+            sdf, geo_feat, beta = h[...,:1], h[...,1:-1], h[...,-1]
+        else:
+            sdf, geo_feat = h[...,:1], h[...,1:-1]
+
+        if embed_pos is not None:
+            rgb = self.color_net(torch.cat([embed_pos, embed_color, geo_feat], dim=-1))
+        else:
+            rgb = self.color_net(torch.cat([embed_color, geo_feat], dim=-1))
+        if config['active']['active']:
+            return torch.cat([rgb, sdf, beta], -1)
+        else:
+            return torch.cat([rgb, sdf], -1)
+
+
+class ColorSDFNet_v2(nn.Module):
+    '''
+    No color grid
+    '''
+
+    def __init__(self, config, input_ch=3, input_ch_pos=12):
+        super(ColorSDFNet_v2, self).__init__()
+        self.config = config
+        self.sdf_net = SDFNet(config,
+                              input_ch=input_ch + input_ch_pos,
+                              geo_feat_dim=config['decoder']['geo_feat_dim'],
+                              hidden_dim=config['decoder']['hidden_dim'],
+                              num_layers=config['decoder']['num_layers'])
+        self.color_net = ColorNet(config,
+                                  input_ch=input_ch_pos,
+                                  geo_feat_dim=config['decoder']['geo_feat_dim'],
+                                  hidden_dim_color=config['decoder']['hidden_dim_color'],
+                                  num_layers_color=config['decoder']['num_layers_color'])
+
+    def forward(self, embed, embed_pos):
+        # embed_pos: [174080,48]
+        # embed: [174080,32]
+
+        if embed_pos is not None:
+            h = self.sdf_net(torch.cat([embed, embed_pos], dim=-1), return_geo=True)
+        else:
+            h = self.sdf_net(embed, return_geo=True)
+
+        sdf, geo_feat = h[..., :1], h[..., 1:-1]
+        if embed_pos is not None:
+            rgb = self.color_net(torch.cat([embed_pos, geo_feat], dim=-1))
+        else:
+            rgb = self.color_net(torch.cat([geo_feat], dim=-1))
+
+        return torch.cat([rgb, sdf], -1)
+
+
+class ColorSDFNet_v3(nn.Module):
+    '''
+    No color grid
+    颜色信息不能直接访问,而是要结合空间编码
+
+    '''
+    def __init__(self, config, input_ch=3, input_ch_pos=12):
+        super(ColorSDFNet_v3, self).__init__()
+        self.config = config
+        self.sdf_net = SDFNet(config,
+                input_ch=input_ch+input_ch_pos,
+                geo_feat_dim=config['decoder']['geo_feat_dim'],
+                hidden_dim=config['decoder']['hidden_dim'],
+                num_layers=config['decoder']['num_layers'])
+        self.color_net = ColorNet(config, 
+                input_ch=input_ch_pos, 
+                geo_feat_dim=config['decoder']['geo_feat_dim'], 
+                hidden_dim_color=config['decoder']['hidden_dim_color'], 
+                num_layers_color=config['decoder']['num_layers_color'])
+
+            
+    def forward(self, embed, embed_pos):
+        # embed_pos: [174080,48]
+        # embed: [174080,32]
+
+        if embed_pos is not None:
+            h = self.sdf_net(torch.cat([embed, embed_pos], dim=-1), return_geo=True)
+        else:
+            h = self.sdf_net(embed, return_geo=True) 
+
+        sdf, geo_feat, beta = h[...,:1], h[...,1:-1], h[...,-1]
+        beta = beta.unsqueeze(-1)
+        if embed_pos is not None:
+            rgb = self.color_net(torch.cat([embed_pos, geo_feat], dim=-1))
+        else:
+            rgb = self.color_net(torch.cat([geo_feat], dim=-1))
+
+        return torch.cat([rgb, sdf, beta], -1)
+
+
+if __name__=='__main__':
+    import torch
+
+    config = {
+        'dataset': 'tum',
+        'data': {'downsample': 1, 'sc_factor': 1, 'translation': 0, 'num_workers': 4,
+                 'datadir': 'data/TUM/rgbd_dataset_freiburg1_desk', 'trainskip': 1,
+                 'output': 'output/TUM/fr_desk', 'exp_name': 'demo'},
+        'mapping': {'sample': 2048, 'first_mesh': True, 'iters': 20, 'cur_frame_iters': 0, 'lr_embed': 0.01,
+                    'lr_decoder': 0.01, 'lr_rot': 0.001, 'lr_trans': 0.001, 'keyframe_every': 5, 'map_every': 5,
+                    'n_pixels': 0.05, 'first_iters': 1000, 'optim_cur': True, 'min_pixels_cur': 100,
+                    'map_accum_step': 1,
+                    'pose_accum_step': 5, 'map_wait_step': 0, 'filter_depth': False,
+                    'bound': [[-3.5, 3], [-3, 3], [-3, 3]], 'marching_cubes_bound': [[-3.5, 3], [-3, 3], [-3, 3]]},
+        'tracking': {'iter': 10, 'sample': 1024, 'pc_samples': 40960, 'lr_rot': 0.01, 'lr_trans': 0.01,
+                     'ignore_edge_W': 20, 'ignore_edge_H': 20, 'iter_point': 0, 'wait_iters': 100, 'const_speed': True,
+                     'best': False},
+        'grid': {'enc': 'HashGrid', 'tcnn_encoding': True, 'hash_size': 16, 'voxel_color': 0.04, 'voxel_sdf': 0.02,
+                 'oneGrid': True}, 'pos': {'enc': 'OneBlob', 'n_bins': 16},
+        'decoder': {'geo_feat_dim': 15, 'hidden_dim': 32, 'num_layers': 2, 'num_layers_color': 2,
+                    'hidden_dim_color': 32,
+                    'tcnn_network': False},
+        'cam': {'H': 480, 'W': 640, 'fx': 517.3, 'fy': 516.5, 'cx': 318.6, 'cy': 255.3, 'png_depth_scale': 5000.0,
+                'crop_edge': 8, 'near': 0, 'far': 5, 'depth_trunc': 5.0, 'crop_size': [384, 512],
+                'distortion': [0.2624, -0.9531, -0.0054, 0.0026, 1.1633]},
+        'training': {'rgb_weight': 1.0, 'depth_weight': 0.1, 'sdf_weight': 5000, 'fs_weight': 10, 'eikonal_weight': 0,
+                     'smooth_weight': 1e-08, 'smooth_pts': 64, 'smooth_vox': 0.04, 'smooth_margin': 0.0,
+                     'n_samples_d': 64,
+                     'range_d': 0.25, 'n_range_d': 21, 'n_importance': 0, 'perturb': 1, 'white_bkgd': False,
+                     'trunc': 0.05,
+                     'rot_rep': 'axis_angle', 'rgb_missing': 1.0},
+        'mesh': {'resolution': 512, 'render_color': False, 'vis': 500, 'voxel_eval': 0.05, 'voxel_final': 0.03,
+                 'visualisation': False},
+        "active":{"active":True},
+        'inherit_from': 'configs/Tum/tum.yaml'
+    }
+    model = ColorSDFNet_v3(config, input_ch=32, input_ch_pos=48)
+    # embed_pos: [174080,48]
+    # embed: [174080,32]
+    embed = torch.randn(174080,32)
+    embed_pos = torch.randn(174080,48)
+    model.forward(embed, embed_pos)
+
+    colornet=ColorNet(config,input_ch=48,geo_feat_dim=config['decoder']['geo_feat_dim'],
+                hidden_dim_color=config['decoder']['hidden_dim_color'],
+                num_layers_color=config['decoder']['num_layers_color'])
+    inputfeat4colornet= torch.randn(174080,63)
+    colornet.forward(inputfeat4colornet)
+
+    sdfnet=SDFNet(config,input_ch=32+48,geo_feat_dim=config['decoder']['geo_feat_dim'],
+                hidden_dim=config['decoder']['hidden_dim'],
+                num_layers=config['decoder']['num_layers'])
+    x4SDFNet= torch.randn(174080,32+48)
+    sdfnet.forward(x4SDFNet)
---
+++ ActiveCoSLAM/ac_scene_rep.py
@@ -0,0 +1,352 @@
+# package imports
+import torch
+import torch.nn as nn
+
+# Local imports
+from model.encodings import get_encoder
+from ActiveCoSLAM.ac_decoder import ColorSDFNet, ColorSDFNet_v3
+from model.utils import sample_pdf, batchify, get_sdf_loss, mse2psnr, compute_loss
+
+class JointEncoding(nn.Module):
+    def __init__(self, config, bound_box):
+        super(JointEncoding, self).__init__()
+        self.config = config
+        self.bounding_box = bound_box
+        self.get_resolution()
+        # 联合编码方案:
+        # 1. parametric encoding用HashGrid
+        # 2. coordinate encoding用OneBlob
+        self.get_encoding(config)
+        self.get_decoder(config)
+        
+
+    def get_resolution(self):
+        '''
+        Get the resolution of the grid
+        '''
+        dim_max = (self.bounding_box[:,1] - self.bounding_box[:,0]).max()
+        if self.config['grid']['voxel_sdf'] > 10:
+            self.resolution_sdf = self.config['grid']['voxel_sdf']
+        else:
+            self.resolution_sdf = int(dim_max / self.config['grid']['voxel_sdf'])
+        
+        if self.config['grid']['voxel_color'] > 10:
+            self.resolution_color = self.config['grid']['voxel_color']
+        else:
+            self.resolution_color = int(dim_max / self.config['grid']['voxel_color'])
+        
+        print('SDF resolution:', self.resolution_sdf)
+
+    def get_encoding(self, config):
+        '''
+        Get the encoding of the scene representation
+        以tum.yaml为例
+        config['pos']['enc']: 'OneBlob'
+        config['grid']['enc']: 'HashGrid'
+        config['grid']['oneGrid']: 'True'
+        '''
+        # Coordinate encoding
+        # get_encoder函数由 tcnn引入,包含了常用的编码方式(四种)
+        self.embedpos_fn, self.input_ch_pos = get_encoder(config['pos']['enc'], n_bins=self.config['pos']['n_bins'])
+
+        # Sparse parametric encoding (SDF)
+        self.embed_fn, self.input_ch = get_encoder(config['grid']['enc'], log2_hashmap_size=config['grid']['hash_size'], desired_resolution=self.resolution_sdf)
+
+        # Sparse parametric encoding (Color)
+        if not self.config['grid']['oneGrid']:
+            print('Color resolution:', self.resolution_color)
+            self.embed_fn_color, self.input_ch_color = get_encoder(config['grid']['enc'], log2_hashmap_size=config['grid']['hash_size'], desired_resolution=self.resolution_color)
+
+    def get_decoder(self, config):
+        '''
+        Get the decoder of the scene representation
+        对应神经网络部分接受编码信息,输出颜色和SDF
+        TODO 输出 beta
+        config['grid']['oneGrid']: 'True'
+        ColorSDFNet只有 SDF Grid
+        ColorSDFNet_v2有 SDF Grid 和 Color Grid
+        '''
+        if not self.config['grid']['oneGrid']:
+            self.decoder = ColorSDFNet(config, input_ch=self.input_ch, input_ch_pos=self.input_ch_pos)
+        else:
+            # CoSLAM with active Learning
+            self.decoder = ColorSDFNet_v3(config, input_ch=self.input_ch, input_ch_pos=self.input_ch_pos)
+        # 对应公式 2, 3 默认调用 v3
+        self.color_net = batchify(self.decoder.color_net, None)
+        self.sdf_net = batchify(self.decoder.sdf_net, None)
+
+    def sdf2weights(self, sdf, z_vals, args=None):
+        '''
+        Convert signed distance function to weights.
+
+        Params:
+            sdf: [N_rays, N_samples]
+            z_vals: [N_rays, N_samples]
+        Returns:
+            weights: [N_rays, N_samples]
+        '''
+        weights = torch.sigmoid(sdf / args['training']['trunc']) * torch.sigmoid(-sdf / args['training']['trunc'])
+
+        signs = sdf[:, 1:] * sdf[:, :-1]
+        mask = torch.where(signs < 0.0, torch.ones_like(signs), torch.zeros_like(signs))
+        inds = torch.argmax(mask, axis=1)
+        inds = inds[..., None]
+        z_min = torch.gather(z_vals, 1, inds) # The first surface
+        mask = torch.where(z_vals < z_min + args['data']['sc_factor'] * args['training']['trunc'], torch.ones_like(z_vals), torch.zeros_like(z_vals))
+
+        weights = weights * mask
+        return weights / (torch.sum(weights, axis=-1, keepdims=True) + 1e-8)
+    
+    def raw2outputs(self, raw, z_vals, white_bkgd=False):
+        '''
+        Perform volume rendering using weights computed from sdf.
+
+        Params:
+            raw: [N_rays, N_samples, 4]
+            z_vals: [N_rays, N_samples]
+        Returns:
+            rgb_map: [N_rays, 3]
+            disp_map: [N_rays]
+            acc_map: [N_rays]
+            weights: [N_rays, N_samples]
+        '''
+        rgb = torch.sigmoid(raw[...,:3])  # [N_rays, N_samples, 3]
+        weights = self.sdf2weights(raw[..., 3], z_vals, args=self.config)
+        rgb_map = torch.sum(weights[...,None] * rgb, -2)  # [N_rays, 3]
+
+        depth_map = torch.sum(weights * z_vals, -1)
+        depth_var = torch.sum(weights * torch.square(z_vals - depth_map.unsqueeze(-1)), dim=-1)
+        disp_map = 1./torch.max(1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
+        acc_map = torch.sum(weights, -1)
+
+        if white_bkgd:
+            rgb_map = rgb_map + (1.-acc_map[...,None])
+
+        return rgb_map, disp_map, acc_map, weights, depth_map, depth_var
+    
+    def query_sdf(self, query_points, return_geo=False, embed=False):
+        '''
+        Get the SDF value of the query points
+        Params:
+            query_points: [N_rays, N_samples, 3]
+        Returns:
+            sdf: [N_rays, N_samples]
+            geo_feat: [N_rays, N_samples, channel]
+        '''
+        inputs_flat = torch.reshape(query_points, [-1, query_points.shape[-1]])
+  
+        embedded = self.embed_fn(inputs_flat)
+        if embed:
+            return torch.reshape(embedded, list(query_points.shape[:-1]) + [embedded.shape[-1]])
+
+        embedded_pos = self.embedpos_fn(inputs_flat)
+        out = self.sdf_net(torch.cat([embedded, embedded_pos], dim=-1))
+        sdf, geo_feat = out[..., :1], out[..., 1:]
+
+        sdf = torch.reshape(sdf, list(query_points.shape[:-1]))
+        if not return_geo:
+            return sdf
+        geo_feat = torch.reshape(geo_feat, list(query_points.shape[:-1]) + [geo_feat.shape[-1]])
+
+        return sdf, geo_feat
+    
+    def query_color(self, query_points):
+        return torch.sigmoid(self.query_color_sdf(query_points)[..., :3])
+      
+    def query_color_sdf(self, query_points):
+        '''
+        Query the color and sdf at query_points.
+
+        Params:
+            query_points: [N_rays, N_samples, 3]
+        Returns:
+            raw: [N_rays, N_samples, 4]
+        '''
+        inputs_flat = torch.reshape(query_points, [-1, query_points.shape[-1]])
+
+        embed = self.embed_fn(inputs_flat)
+        embe_pos = self.embedpos_fn(inputs_flat)
+        if not self.config['grid']['oneGrid']:
+            embed_color = self.embed_fn_color(inputs_flat)
+            return self.decoder(embed, embe_pos, embed_color)
+        return self.decoder(embed, embe_pos)
+    
+    def run_network(self, inputs):
+        """
+        Run the network on a batch of inputs.
+
+        Params:
+            inputs: [N_rays, N_samples, 3]
+        Returns:
+            outputs: [N_rays, N_samples, 4]
+        """
+        inputs_flat = torch.reshape(inputs, [-1, inputs.shape[-1]])
+        
+        # Normalize the input to [0, 1] (TCNN convention)
+        if self.config['grid']['tcnn_encoding']:
+            inputs_flat = (inputs_flat - self.bounding_box[:, 0]) / (self.bounding_box[:, 1] - self.bounding_box[:, 0])
+
+        outputs_flat = batchify(self.query_color_sdf, None)(inputs_flat)
+        outputs = torch.reshape(outputs_flat, list(inputs.shape[:-1]) + [outputs_flat.shape[-1]])
+
+        return outputs
+    
+    def render_surface_color(self, rays_o, normal):
+        '''
+        Render the surface color of the points.
+        Params:
+            points: [N_rays, 1, 3]
+            normal: [N_rays, 3]
+        '''
+        n_rays = rays_o.shape[0]
+        trunc = self.config['training']['trunc']
+        z_vals = torch.linspace(-trunc, trunc, steps=self.config['training']['n_range_d']).to(rays_o)
+        z_vals = z_vals.repeat(n_rays, 1)
+        # Run rendering pipeline
+        
+        pts = rays_o[...,:] + normal[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples, 3]
+        raw = self.run_network(pts)
+        rgb, disp_map, acc_map, weights, depth_map, depth_var = self.raw2outputs(raw, z_vals, self.config['training']['white_bkgd'])
+        return rgb
+    
+    def render_rays(self, rays_o, rays_d, target_d=None):
+        '''在 forward 函数中调用
+        Params:
+            rays_o: [N_rays, 3]
+            rays_d: [N_rays, 3]
+            target_d: [N_rays, 1]
+
+        '''
+        # ----------------- 光线采样 -----------------
+        n_rays = rays_o.shape[0]
+        #
+        # Sample depth
+        if target_d is not None:
+            # 如果有目标深度,则在目标深度[-range_d, range_d]范围内均匀/等间隔采样21个点
+            # range_d: 0.25
+            # n_range_d: 21
+            z_samples = torch.linspace(-self.config['training']['range_d'], self.config['training']['range_d'], steps=self.config['training']['n_range_d']).to(target_d) 
+            z_samples = z_samples[None, :].repeat(n_rays, 1) + target_d
+            # squeeze()函数是为了去除张量中维度为1的维度
+            # 对于目标深度为负值的点：
+            # 在深度near到far的范围内生成n_range_d个等间隔的张量。
+            # 深度值被赋给z_samples中相应位置，覆盖原先的目标深度为负值的部分。
+            # 使用前的张量是：
+            # tensor([[1],
+            #         [2],
+            #         [3]])
+            # 使用squeeze()后得到的张量是：
+            # tensor([1, 2, 3])
+            z_samples[target_d.squeeze()<=0] = torch.linspace(self.config['cam']['near'], self.config['cam']['far'], steps=self.config['training']['n_range_d']).to(target_d)
+
+            if self.config['training']['n_samples_d'] > 0:
+                z_vals = torch.linspace(self.config['cam']['near'], self.config['cam']['far'], self.config['training']['n_samples_d'])[None, :].repeat(n_rays, 1).to(rays_o)
+                z_vals, _ = torch.sort(torch.cat([z_vals, z_samples], -1), -1)
+            else:
+                z_vals = z_samples
+        else: # 没有  target_d
+            z_vals = torch.linspace(self.config['cam']['near'], self.config['cam']['far'], self.config['training']['n_samples']).to(rays_o)
+            z_vals = z_vals[None, :].repeat(n_rays, 1) # [n_rays, n_samples]
+
+        # Perturb sampling depths 给深度加入噪音
+        if self.config['training']['perturb'] > 0.:
+            mids = .5 * (z_vals[...,1:] + z_vals[...,:-1])
+            upper = torch.cat([mids, z_vals[...,-1:]], -1)
+            lower = torch.cat([z_vals[...,:1], mids], -1)
+            z_vals = lower + (upper - lower) * torch.rand(z_vals.shape).to(rays_o)
+
+        # 将点输入到神经网络进行渲染
+        pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples, 3]
+        raw = self.run_network(pts)
+        # TODO 更改返回
+        rgb_map, disp_map, acc_map, weights, depth_map, depth_var = self.raw2outputs(raw, z_vals, self.config['training']['white_bkgd'])
+
+        # Importance sampling
+        # 默认不执行
+        if self.config['training']['n_importance'] > 0:
+
+            rgb_map_0, disp_map_0, acc_map_0, depth_map_0, depth_var_0 = rgb_map, disp_map, acc_map, depth_map, depth_var
+
+            z_vals_mid = .5 * (z_vals[...,1:] + z_vals[...,:-1])
+            z_samples = sample_pdf(z_vals_mid, weights[...,1:-1], self.config['training']['n_importance'], det=(self.config['training']['perturb']==0.))
+            z_samples = z_samples.detach()
+
+            z_vals, _ = torch.sort(torch.cat([z_vals, z_samples], -1), -1)
+            pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples + N_importance, 3]
+
+            raw = self.run_network(pts)
+            rgb_map, disp_map, acc_map, weights, depth_map, depth_var = self.raw2outputs(raw, z_vals, self.config['training']['white_bkgd'])
+
+        # Return rendering outputs
+        ret = {'rgb' : rgb_map, 'depth' :depth_map, 
+               'disp_map' : disp_map, 'acc_map' : acc_map, 
+               'depth_var':depth_var,}
+        ret = {**ret, 'z_vals': z_vals}
+
+        ret['raw'] = raw
+
+        if self.config['training']['n_importance'] > 0:
+            ret['rgb0'] = rgb_map_0
+            ret['disp0'] = disp_map_0
+            ret['acc0'] = acc_map_0
+            ret['depth0'] = depth_map_0
+            ret['depth_var0'] = depth_var_0
+            ret['z_std'] = torch.std(z_samples, dim=-1, unbiased=False)
+
+        return ret
+    
+    def forward(self, rays_o, rays_d, target_rgb, target_d, global_step=0):
+        '''
+        Params:
+            rays_o: ray origins (Bs, 3)
+            rays_d: ray directions (Bs, 3)
+            frame_ids: use for pose correction (Bs, 1)
+            target_rgb: rgb value (Bs, 3)
+            target_d: depth value (Bs, 1)
+            c2w_array: poses (N, 4, 4) 
+             r r r tx
+             r r r ty
+             r r r tz
+        '''
+
+        # Get render results
+        rend_dict = self.render_rays(rays_o, rays_d, target_d=target_d)
+
+        if not self.training:
+            #如果不在训练则直接返回
+            return rend_dict
+        
+        # Get depth and rgb weights for loss
+        valid_depth_mask = (target_d.squeeze() > 0.) * (target_d.squeeze() < self.config['cam']['depth_trunc'])
+        rgb_weight = valid_depth_mask.clone().unsqueeze(-1)
+        rgb_weight[rgb_weight==0] = self.config['training']['rgb_missing']
+
+        # Get render loss
+        ## 1.  rgb_loss/depth_loss对应公式 6, 单纯的 L2 loss
+        rgb_loss = compute_loss(rend_dict["rgb"]*rgb_weight, target_rgb*rgb_weight)
+        psnr = mse2psnr(rgb_loss)
+        depth_loss = compute_loss(rend_dict["depth"].squeeze()[valid_depth_mask], target_d.squeeze()[valid_depth_mask])
+
+        if 'rgb0' in rend_dict:
+            #loss 会计算粗网络和细网络的和
+            rgb_loss += compute_loss(rend_dict["rgb0"]*rgb_weight, target_rgb*rgb_weight)
+            depth_loss += compute_loss(rend_dict["depth0"][valid_depth_mask], target_d.squeeze()[valid_depth_mask])
+        
+        ## 2. Get sdf loss/free space loss 公式七
+        z_vals = rend_dict['z_vals']  # [N_rand, N_samples + N_importance]
+        sdf = rend_dict['raw'][..., -1]  # [N_rand, N_samples + N_importance]
+        truncation = self.config['training']['trunc'] * self.config['data']['sc_factor']
+        fs_loss, sdf_loss = get_sdf_loss(z_vals, target_d, sdf, truncation, 'l2', grad=None)         
+        
+        ## TODO 更改 loss
+        ret = {
+            "rgb": rend_dict["rgb"],
+            "depth": rend_dict["depth"],
+            "rgb_loss": rgb_loss,
+            "depth_loss": depth_loss,
+            "sdf_loss": sdf_loss,
+            "fs_loss": fs_loss,
+            "psnr": psnr,
+        }
+
+        return ret
